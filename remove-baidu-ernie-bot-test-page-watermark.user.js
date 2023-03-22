911163aa71b572cbddae385461357f63873f3785
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-03-22 14:24:27 +0800

    Update remove-baidu-ernie-bot-test-page-watermark.user.js

--- remove-baidu-ernie-bot-test-page-watermark.user.js
+++ remove-baidu-ernie-bot-test-page-watermark.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Remove Baidu ERNIE Bot Test Page Watermark
+// @name         移除百度文心一言测试页水印 Remove Baidu ERNIE Bot Test Page Watermark
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Why do they put watermark on garbage?
 // @author       Bing Chat & Me
 // @match        https://yiyan.baidu.com/
@@ -10,27 +10,28 @@
 // @license      WTFPL
 // ==/UserScript==
 
+
+// Save the original MutationObserver constructor
+const OriginalMutationObserver = window.MutationObserver;
+
+// Define a custom MutationObserver constructor that does nothing
+function CustomMutationObserver(callback) {
+    // Do nothing
+    return {
+        observe: function () {},
+        disconnect: function () {},
+        takeRecords: function () { return []; }
+    };
+}
+
+// Assign the custom constructor to the window object
+window.MutationObserver = CustomMutationObserver;
+
+// Optionally, you can restore the original constructor later if needed
+// window.MutationObserver = OriginalMutationObserver;
+
 (function() {
     'use strict';
-
-    // Save the original MutationObserver constructor
-    const OriginalMutationObserver = window.MutationObserver;
-
-    // Define a custom MutationObserver constructor that does nothing
-    function CustomMutationObserver(callback) {
-        // Do nothing
-        return {
-            observe: function () {},
-            disconnect: function () {},
-            takeRecords: function () { return []; }
-        };
-    }
-
-    // Assign the custom constructor to the window object
-    window.MutationObserver = CustomMutationObserver;
-
-    // Optionally, you can restore the original constructor later if needed
-    // window.MutationObserver = OriginalMutationObserver;
 
     function delWM(){
         // 获取所有有ShadowRoot的元素
